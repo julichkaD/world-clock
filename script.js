a955b101b544429260fd5aeda789b921e88e00d5
Julia Darwish <darwishjulia4@gmail.com>
2023-11-13 11:48:58 -0500

    Current location added

--- script.js
+++ script.js
@@ -25,6 +25,9 @@ selectCity.addEventListener("change", updateCity);
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
@@ -41,7 +44,3 @@ function updateCity(event) {
 }
 updateCity();
 setInterval(updateCity, 1000);
-
-
-
-
